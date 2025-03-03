f55281d5bc080824b42ca5a6eff9b81fe936a357
Alex Chavez <achavez25@wooster.edu>
2025-03-03 15:23:03 -0500

    fixed tailwindcss file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,13 @@
-import localFont from "next/font/local";
-import "./globals.css";
+import "./globals.css"
 import { Inter } from "next/font/google"
-import React from "react";
 
 const inter = Inter({ subsets: ["latin"] })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata = {
-  title: "Alex Chavez",
-  description: "Portfolio website of Alex Chavez, a software engineer specializing in AI, game development, and full-stack development.",
-};
+  title: "Alex Chavez | Software Engineer",
+  description:
+    "Portfolio website of Alex Chavez, a software engineer specializing in AI, game development, and full-stack development.",
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -28,3 +16,4 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./pages/**/*.{js,jsx,mdx}",
-    "./components/**/*.{js,jsx,mdx}",
-    "./app/**/*.{js,jsx,mdx}",
-    "*.{js,jsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+  content: [ 
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",    
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",    
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",    
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
     container: {

e60e194adf63fcab0147d0d4b52b0ebe4edcf1c5
Alex Chavez <achavez25@wooster.edu>
2025-04-03 22:12:15 -0400

    added image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // ensures it's fully static
+    basePath: '/website_second', // important for GitHub Pages
+    images: {
+      unoptimized: true, // disables next/image optimization (required for static exports)
+    },
+  };
+  
+  module.exports = nextConfig;
+  
